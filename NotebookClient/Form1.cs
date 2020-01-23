abb3c94cd5a887d6644a2df4be1c5029df0ec652
katka6622@gmail.com <katka6622@gmail.com>
2020-01-23 12:35:32 +0500

    Добавлен первичный клиент

---
+++ NotebookClient/Form1.cs
@@ -0,0 +1,126 @@
+﻿using NotebookAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotebookClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const string _baseAddress = "http://localhost:2728/";
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Update();
+        }
+        private void Update()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_baseAddress);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new
+                MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response;
+                response = client.GetAsync("api/People").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Person[] reports =
+                    response.Content.ReadAsAsync<Person[]>().Result;
+                    foreach (var p in reports)
+                    {
+                        var item = new ListViewItem(new[] { p.Firstname, p.Secondname, p.BirthDay.ToShortDateString() });
+                        item.Tag = p.Id;
+
+                        listView.Items.Add(item);
+                    }
+                }
+            }
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Add();
+        }
+        private void Add()
+        {
+            Person newReport = new Person()
+            {
+                Firstname = "Коля",
+                Secondname = "Иванов",
+                BirthDay =
+                  DateTime.Parse("01.02.2003")
+            };
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_baseAddress);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new
+                MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.PostAsJsonAsync("api/People", newReport).Result;
+            }
+        }
+
+        private void Delete(int delete)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:2728/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.DeleteAsync("api/People/" + delete).Result;
+            }
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count != 0)
+            {
+                int id = (int)listView.SelectedItems[0].Tag;
+                Delete(id);
+                Update();
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate2_Click(object sender, EventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_baseAddress);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new
+                MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response;
+                response = client.GetAsync("api/Contact").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Person[] reports =
+                    response.Content.ReadAsAsync<Person[]>().Result;
+                    foreach (var p in reports)
+                    {
+                        var item = new ListViewItem(new[] { p.Firstname, p.Secondname, p.BirthDay.ToShortDateString() });
+                        item.Tag = p.Id;
+
+                        listView.Items.Add(item);
+                    }
+                }
+            }
+        }
+    }
+}
